8016da6a0ff02593df5bc04db1b1cd91e4004cc2
David Kovalov <kovalovdavid92@gmail.com>
2024-12-04 10:37:10 +0200

    file have 2 types of operations + -

---
+++ app.py
@@ -0,0 +1,8 @@
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+choose_operation = input("Choose operation: ")
+
+if choose_operation == "+":
+    print(first_num + second_num)
+elif choose_operation == "-":
+    print(first_num - second_num)
